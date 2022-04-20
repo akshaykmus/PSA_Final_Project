cfef30668637e03dc00b2c06d0505a9fe8359efd
akshaykmus <akshaykmus@gmail.com>
2022-04-20 00:08:37 -0400

    Added easybot.

---
+++ src/main/java/com/neu/psa/tic/tac/toe/game/EasyMenace.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.neu.psa.tic.tac.toe.game;
+import java.util.Random;
+import javax.swing.JButton;
+/**
+ *
+ * @author akshaykumthimatad
+ */
+public class EasyMenace {
+    
+    public int easyMove(JButton[] buttons){
+        Random ran = new Random();
+        // check if place is empty;if true then return index of empty space.
+        // TODO: check for Draw
+        int a = ran.nextInt(9);
+        while(buttons[a].getText()!=""){
+        a = ran.nextInt(9);
+        }
+        return a;
+   
+       
+        
+    }
+    
+}
