3a845a346f2b6f8d7859988e835ad15a768e0ecf
akshaykmus <akshaykmus@gmail.com>
2022-04-22 18:10:01 -0400

    Akshay | Added Excel funcs.

---
+++ src/main/java/com/neu/psa/tic/tac/toe/game/Excelutil.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.neu.psa.tic.tac.toe.game;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+/**
+ *
+ * @author akshaykumthimatad
+ */
+public class Excelutil {
+
+    public static void main(String[] args) throws IOException {
+        writeData(1,2,3);
+        getData(1, 2);
+
+    }
+
+    public static double getData(int value1, int value2) {
+        double value = 0;
+        try {
+            String path = "./src/main/java/com/neu/psa/tic/tac/toe/game/ExcelUtils/data.xlsx";
+            XSSFWorkbook work = new XSSFWorkbook(path);
+            XSSFSheet sheet = work.getSheet("Sheet1");
+            value = sheet.getRow(value1).getCell(value2).getNumericCellValue();
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        //System.out.print(value);
+        return value;
+    }
+
+    public static void writeData(int row, int col, int value) throws IOException {
+        String path = "./src/main/java/com/neu/psa/tic/tac/toe/game/ExcelUtils/data.xlsx";
+        XSSFWorkbook work = new XSSFWorkbook(path);
+        XSSFSheet sheet = work.getSheet("Sheet1");
+        sheet.getRow(row).getCell(col).setCellValue(value);
+        FileOutputStream fos = new FileOutputStream(path);
+        work.write(fos);
+        fos.flush();
+        fos.close();
+
+        System.out.print(value);
+    }
+
+}
